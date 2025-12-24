a76007dcfac4eb66fb232e2b43ae17d6771bd55f
Davide Caruso <d.caruso4@studenti.unipi.it>
2025-12-24 18:10:01 +0100

    Delete saxon directory

--- saxon/EseguiSaxon.bat
+++
@@ -1,4 +0,0 @@
-@Pushd "%~dp0"
-@Rem java -jar F:\SaxonHE\saxon-he-12.9.jar -s:testo.xml -xsl:tei_transform.xsl -o:output.html --standardErrorOutputFile:errori.txt
-java -jar F:\SaxonHE\saxon-he-12.9.jar -s:testo.xml -xsl:tei_transform.xsl -o:index.html
-@Pause
